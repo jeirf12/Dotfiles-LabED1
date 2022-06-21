130687d295fe9311feb060962cc6574e33776d41
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2022-06-21 18:38:28 -0500

    news settings for 'consola'
    Content news rules for method 'leer' of generic form

--- Practica 2/src/Logica/Consola.java
+++ Practica 2/src/Logica/Consola.java
@@ -4,61 +4,63 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Consola {
-	public static <T> boolean escribirSaltarLinea (T parEtiqueta,boolean parDeseaMensaje) {
+	public static <T> boolean escribirSaltarLinea (T parEtiqueta,boolean parWichMessage) {
 		try {
 			System.out.println(parEtiqueta);
-			if (parDeseaMensaje) {
+			if (parWichMessage) {
 				System.out.println("Se escribio correctamente");
 			}
 			return true;
 		} catch (Exception e) {
-			if (parDeseaMensaje) {
+			if (parWichMessage) {
 				System.out.println("no se pudo escribir correctamente debido a: "+e.getMessage());
 			}
 			return false;
 		}
 	}
-	public static <T> boolean escribir(T parEtiqueta,boolean parDeseaMensaje) {
+	public static <T> boolean escribir(T parEtiqueta,boolean parWichMessage) {
 		try {
 			System.out.print(parEtiqueta);
-			if (parDeseaMensaje) {
+			if (parWichMessage) {
 				System.out.print("Se escribio correctamente");
 			}
 			return true;
 		} catch (Exception e) {
-			if (parDeseaMensaje) {
+			if (parWichMessage) {
 				System.out.print("no se pudo escribir correctamente debido a: "+e.getMessage());
 			}
 			return false;
 		}
 	}
 	
-	@SuppressWarnings("unchecked")
-	public static <T> T leer(T parVarlectura,boolean parDeseaMensaje) {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		String valor="";
+	private static <T> T leer(T parRead, boolean parWichMessage) {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		String value = "", option = "";
+		T parBackupRead = parRead;
 		try {
-			valor=br.readLine();
-			if (!esNumerico(""+parVarlectura) && !esNumerico(valor)) {
-				parVarlectura = (T) valor; 
-			} else if(esNumerico(""+parVarlectura) && esNumerico(valor)) {
-				parVarlectura=(T) Integer.valueOf(valor);
+			value = input.readLine();
+			option =  ((Object) parRead).getClass().getSimpleName();
+			switch(option){
+				case "String" -> { if(!isNumber("" + parRead)) parRead = (T) String.valueOf(value.trim()); }
+				case "Double" -> { if(isNumber("" + parRead) && value.contains(".")) parRead = (T) Double.valueOf(value); }
+				case "Float" -> { if(isNumber("" + parRead) && value.contains(".")) parRead = (T) Float.valueOf(value); }
+				case "Integer" -> { if(isNumber("" + parRead)) parRead = (T) Integer.valueOf(value); }
 			}
-			if (parDeseaMensaje) {
+			if (parWichMessage) {
 				System.out.print("Se leyo correctamente");
 			}
 		} catch (Exception e) {
-			if (parDeseaMensaje) {
+			if (parWichMessage) {
 				System.out.println("No se pudo leer debido a el error: "+e.getMessage());
 			}
-			parVarlectura.getClass().isInstance(valor.getClass());
 		}
-		return parVarlectura;
+		if(parBackupRead.equals(parRead)) escribir("Value introduced no valid", parWichMessage);
+		return parRead;
 	}
 	
-	public static <T> T leer(String parEtiqueta,T parVariable,boolean parDeseaMensaje) {
-		escribir(parEtiqueta, parDeseaMensaje);
-		return leer(parVariable, parDeseaMensaje);
+	public static <T> T leer(String parEtiqueta,T parVariable,boolean parWichMessage) {
+		escribir(parEtiqueta, parWichMessage);
+		return leer(parVariable, parWichMessage);
 	}
 	public static <T> boolean leer(T [] parVector) {
 		for (int varIndice = 0; varIndice < parVector.length; varIndice++) {
@@ -82,7 +84,7 @@ public class Consola {
 		}
 		return true;
 	}
-	public static boolean esNumerico(String dato) {
-		return dato.matches("[+-]?\\d*(\\.\\d+)?");
+	public static boolean isNumber(String chain) {
+		return chain.matches("[+-]?\\d*(\\.\\d+)?");
 	}
 }
