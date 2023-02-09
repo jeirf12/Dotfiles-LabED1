f71c926e852e6537eeda11f00216332feccdcba6
jeirf12 <jhonfer.ruiz@gmail.com>
2023-02-08 20:41:14 -0500

    updated code Practice 4

--- Practica 4/src/Negocio/ArbolBinario.java
+++ Practica 4/src/Negocio/ArbolBinario.java
@@ -3,87 +3,60 @@ package Negocio;
 public class ArbolBinario<T> {
 	private Nodo<T> raiz;
 
-	public ArbolBinario() {
-		this.raiz = null;
-	}
+	public ArbolBinario() { this.raiz = null; }
 
-	public Nodo<T> getRaiz() {
-		return raiz;
-	}
+	public Nodo<T> getRaiz() { return this.raiz; }
 
-	public void setRaiz(Nodo<T> raiz) {
-		this.raiz = raiz;
-	}
+	public void setRaiz(Nodo<T> raiz) { this.raiz = raiz; }
 
-	public boolean buscarElemento(T elemento) {
-		return buscarElemento(raiz, elemento);
-	}
+	public boolean buscarElemento(T elemento) { return this.buscarElemento(this.raiz, elemento); }
 
-	public boolean buscarElemento(Nodo<T> raiz, T elemento) {
-		if (raiz == null) {
-			return false;
-		}
-		if (raiz.getElemento().equals(elemento)) {
-			return true;
-		}
-		if (buscarElemento(raiz.getIzq(), elemento)) {
-			return true;
-		} else if (buscarElemento(raiz.getDer(), elemento)) {
-			return true;
-		}
+	private boolean buscarElemento(Nodo<T> aux, T elemento) {
+		if (aux == null) return false;
+		if (aux.getElemento().equals(elemento)) return true;
+		if (this.buscarElemento(aux.getIzq(), elemento)) return true;
+		else if (this.buscarElemento(aux.getDer(), elemento)) return true;
 		return false;
 	}
 
-	public void mostrarArbolSangrado() {
-		mostrarArbolSangrado(raiz, 0);
-	}
+	public void mostrarArbolSangrado() { this.mostrarArbolSangrado(this.raiz, 0); }
 
-	public void mostrarArbolSangrado(Nodo<T> arbol, int nivel) {
+	private void mostrarArbolSangrado(Nodo<T> arbol, int nivel) {
 		if (arbol != null) {
-			mostrarArbolSangrado(arbol.getIzq(), nivel + 1);
+			this.mostrarArbolSangrado(arbol.getIzq(), nivel + 1);
 			System.out.println(" " + nivel + " " + arbol.getElemento());
-			mostrarArbolSangrado(arbol.getDer(), nivel + 1);
+			this.mostrarArbolSangrado(arbol.getDer(), nivel + 1);
 		}
 	}
-	public Nodo<T> retornarNodo(T elemento){
-		return retornarNodo(raiz, elemento);
-	}
-	public Nodo<T> retornarNodo(Nodo<T> raiz,T elemento){
-		if (raiz != null) {
-			if (raiz.getElemento() == elemento) {
-				return raiz;
-			}
-			if (buscarElemento(raiz.getIzq(), elemento)) {
-				return retornarNodo(raiz.getIzq(), elemento);
-			} else if (buscarElemento(raiz.getDer(), elemento)) {
-				return retornarNodo(raiz.getDer(),elemento);
-			}
+	
+	public Nodo<T> retornarNodo(T elemento) { return this.retornarNodo(this.raiz, elemento); }
+
+	private Nodo<T> retornarNodo(Nodo<T> aux, T elemento) {
+		if (aux != null) {
+			if (aux.getElemento() == elemento) return aux;
+			if (this.buscarElemento(aux.getIzq(), elemento)) return this.retornarNodo(aux.getIzq(), elemento);
+			else if (this.buscarElemento(aux.getDer(), elemento)) return this.retornarNodo(aux.getDer(),elemento);
 		}
 		return null;
 	}
 	
-	public void insertar(T elemento){
-		raiz=new Nodo<T>(elemento);
-	}
+	public void insertar(T elemento) { this.raiz = new Nodo<T>(elemento); }
+
 	public void insertar(T padre,T hijo,String tHijo){
 		Nodo<T> temp;
-		if (raiz!=null) {
-			temp=retornarNodo(padre);
-			temp=temp.insertarNodo(temp, hijo, tHijo);
+		if (this.raiz != null) {
+			temp = retornarNodo(padre);
+			temp.insertarNodo(temp, hijo, tHijo);
 		}
 	}
+
 	public void eliminar(T padre,T elemento) {
-		if (padre!=elemento) {
+		if (padre != elemento) { 
 			if (buscarElemento(elemento) && buscarElemento(padre)) {
-				Nodo<T> aux=retornarNodo(padre);
-				if (aux.getIzq().getElemento()==elemento) {
-					aux.setIzq(null);
-				}else if (aux.getDer().getElemento()==elemento) {
-					aux.setDer(null);
-				}
+				Nodo<T> aux = retornarNodo(padre);
+				if (aux.getIzq().getElemento() == elemento) aux.setIzq(null);
+				else if (aux.getDer().getElemento() == elemento) aux.setDer(null);
 			}
-		}else {
-			raiz=null;
-		}
+		} else this.raiz = null;
 	}
 }
--- Practica 4/src/Presentacion/Test.java
+++ Practica 4/src/Presentacion/Test.java
@@ -8,16 +8,15 @@ public class Test {
 		ArbolBinario<Integer> ObjetoArbol = new ArbolBinario<Integer>();
 		
 		ObjetoArbol.insertar(3);
-		ObjetoArbol.insertar(3,4,"I");
-		ObjetoArbol.insertar(3,1,"D");
-		ObjetoArbol.insertar(4,2,"I");
-		ObjetoArbol.insertar(4,5,"D");
-		ObjetoArbol.insertar(1,9,"I");
-		ObjetoArbol.insertar(1,8,"D");
-		ObjetoArbol.eliminar(1,8);
-		//mostrar el árbol resultante
-		ObjetoArbol.mostrarArbolSangrado();
-		
-	}
+		ObjetoArbol.insertar(3, 4, "I");
+		ObjetoArbol.insertar(3, 1, "D");
+		ObjetoArbol.insertar(4, 2, "I");
+		ObjetoArbol.insertar(4, 5, "D");
+		ObjetoArbol.insertar(1, 9, "I");
+		ObjetoArbol.insertar(1, 8, "D");
+		ObjetoArbol.eliminar(1, 8);
 
+		//mostrar el ï¿½rbol resultante
+		ObjetoArbol.mostrarArbolSangrado();
+	}
 }
