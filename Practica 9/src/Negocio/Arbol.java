79a62a96d2a09538fccd69d74f375c5b9266b220
jeirf12 <jhonfer.ruiz@gmail.com>
2023-02-10 10:14:08 -0500

    updated code Practice 9

--- Practica 9/src/Negocio/Arbol.java
+++ Practica 9/src/Negocio/Arbol.java
@@ -1,136 +1,111 @@
 package Negocio;
 
-public class Arbol <T>{
+public class Arbol <T> {
 	private Nodo<T> raiz;
 
-	public Arbol() {
-		this.raiz = null;
-	}
+	public Arbol() { this.raiz = null; }
 
-	public Nodo<T> getRaiz() {
-		return raiz;
-	}
+	public Nodo<T> getRaiz() { return this.raiz; }
 
-	public void insertar(Comparable<T> elemento) {
-		raiz = insertar(raiz, elemento);
-	}
 
 	public int factorEquilibrio(Nodo<T> arbol) {
-		if (arbol == null)
-			return 0;
-		else
-			return (arbol.getFactorEquilibrio() + 1);
+		if (arbol == null) return 0;
+		else return (arbol.getFactorEquilibrio() + 1);
 	}
 
 	private int mFE(int alturaIzquierdo, int alturaDerecho) {
-		if (alturaIzquierdo > alturaDerecho)
-			return alturaIzquierdo;
-		else
-			return alturaDerecho;
+		if (alturaIzquierdo > alturaDerecho) return alturaIzquierdo;
+		else return alturaDerecho;
 	}
+
+	public void insertar(Comparable<T> elemento) { this.raiz = this.insertar(this.raiz, elemento); }
 
 	@SuppressWarnings("unchecked")
 	private Nodo<T> insertar(Nodo<T> arbol, Comparable<T> elemento) {
-		if (arbol == null) {
-			arbol = new Nodo<T>((T) elemento, null, null);
-		} else if (elemento.compareTo(arbol.getElemento()) < 0) {
+		if (arbol == null) arbol = new Nodo<T>((T) elemento, null, null);
+		else if (elemento.compareTo(arbol.getElemento()) < 0) {
 			arbol.setIzq(insertar(arbol.getIzq(), elemento));
-			if (factorEquilibrio(arbol.getDer()) - factorEquilibrio(arbol.getIzq()) == -2) {
-				if (elemento.compareTo(arbol.getIzq().getElemento()) < 0)
-					arbol = RotacionSimpleDer(arbol);
-				else
-					arbol = RotacionDobleIzq_Der(arbol);
+			if (this.factorEquilibrio(arbol.getDer()) - this.factorEquilibrio(arbol.getIzq()) == -2) {
+				if (elemento.compareTo(arbol.getIzq().getElemento()) < 0) arbol = this.rotacionSimpleDer(arbol);
+				else arbol = this.rotacionDobleIzq_Der(arbol);
 			}
 		} else if (elemento.compareTo(arbol.getElemento()) > 0) {
 			arbol.setDer(insertar(arbol.getDer(), elemento));
-			if (factorEquilibrio(arbol.getDer()) - factorEquilibrio(arbol.getIzq()) == 2) {
-				if (elemento.compareTo(arbol.getDer().getElemento()) > 0)
-					arbol = RotacionSimpleIzq(arbol);
-				else
-					arbol = RotacionDobleDer_Izq(arbol);
+			if (this.factorEquilibrio(arbol.getDer()) - this.factorEquilibrio(arbol.getIzq()) == 2) {
+				if (elemento.compareTo(arbol.getDer().getElemento()) > 0) arbol = this.rotacionSimpleIzq(arbol);
+				else arbol = this.rotacionDobleDer_Izq(arbol);
 			}
 		} else {
 			;// En el caso de que sean valores duplicados no hace nada
 		}
-		arbol.setFactorEquilibrio(mFE(factorEquilibrio(arbol.getIzq()), factorEquilibrio(arbol.getDer())));
+		arbol.setFactorEquilibrio(this.mFE(this.factorEquilibrio(arbol.getIzq()), this.factorEquilibrio(arbol.getDer())));
 		return arbol;
 	}
 
-	private Nodo<T> RotacionSimpleDer(Nodo<T> arbol) {
+	private Nodo<T> rotacionSimpleDer(Nodo<T> arbol) {
 		Nodo<T> subArbol = arbol.getIzq();
 		arbol.setIzq(subArbol.getDer());
 		subArbol.setDer(arbol);
-		arbol.setFactorEquilibrio(mFE(factorEquilibrio(arbol.getIzq()), factorEquilibrio(arbol.getDer())));
-		subArbol.setFactorEquilibrio(mFE(factorEquilibrio(subArbol.getIzq()), arbol.getFactorEquilibrio()));
+		arbol.setFactorEquilibrio(this.mFE(this.factorEquilibrio(arbol.getIzq()), this.factorEquilibrio(arbol.getDer())));
+		subArbol.setFactorEquilibrio(this.mFE(this.factorEquilibrio(subArbol.getIzq()), arbol.getFactorEquilibrio()));
 		return subArbol;
 	}
 
-	private Nodo<T> RotacionSimpleIzq(Nodo<T> arbol) {
+	private Nodo<T> rotacionSimpleIzq(Nodo<T> arbol) {
 		Nodo<T> subArbol = arbol.getDer();
 		arbol.setDer(subArbol.getIzq());
 		subArbol.setIzq(arbol);
-		arbol.setFactorEquilibrio(mFE(factorEquilibrio(arbol.getIzq()), factorEquilibrio(arbol.getDer())));
-		subArbol.setFactorEquilibrio(mFE(factorEquilibrio(subArbol.getDer()), arbol.getFactorEquilibrio()));
+		arbol.setFactorEquilibrio(this.mFE(this.factorEquilibrio(arbol.getIzq()), this.factorEquilibrio(arbol.getDer())));
+		subArbol.setFactorEquilibrio(this.mFE(this.factorEquilibrio(subArbol.getDer()), arbol.getFactorEquilibrio()));
 		return subArbol;
 	}
 
-	private Nodo<T> RotacionDobleIzq_Der(Nodo<T> arbol) {
-		arbol.setIzq(RotacionSimpleIzq(arbol.getIzq()));
-		return RotacionSimpleDer(arbol);
+	private Nodo<T> rotacionDobleIzq_Der(Nodo<T> arbol) {
+		arbol.setIzq(this.rotacionSimpleIzq(arbol.getIzq()));
+		return this.rotacionSimpleDer(arbol);
 	}
 
-	private Nodo<T> RotacionDobleDer_Izq(Nodo<T> subArbol) {
-		subArbol.setDer(RotacionSimpleDer(subArbol.getDer()));
-		return RotacionSimpleIzq(subArbol);
+	private Nodo<T> rotacionDobleDer_Izq(Nodo<T> subArbol) {
+		subArbol.setDer(this.rotacionSimpleDer(subArbol.getDer()));
+		return this.rotacionSimpleIzq(subArbol);
 	}
-	public void imprimirArbol(){
-		imprimirArbol(raiz, 0);
-	}
-	private void imprimirArbol(Nodo<T> arbol,int nivel){
-		if (arbol!=null) {
-			imprimirArbol(arbol.getIzq(),nivel+1);
-			System.out.println(" "+nivel+" "+arbol.getElemento());
-			imprimirArbol(arbol.getDer(), nivel+1);
+
+	public void imprimirArbol(){ this.imprimirArbol(this.raiz, 0); }
+
+	private void imprimirArbol(Nodo<T> arbol, int nivel) {
+		if (arbol != null) {
+			this.imprimirArbol(arbol.getIzq(), nivel + 1);
+			System.out.println(" " + nivel + " " + arbol.getElemento());
+			this.imprimirArbol(arbol.getDer(), nivel + 1);
 		}
 	}
-	public int funcionLucas(int n){
-		if (n==0) {
-			return 2;
-		}else if (n==1) {
-			return 1;
-		}else{
-			return funcionLucas(n-1)+funcionLucas(n-2);
-		}
+
+	private int funcionLucas(int n) {
+		if (n == 0) return 2;
+		else if (n == 1) return 1;
+		else return this.funcionLucas(n - 1) + this.funcionLucas(n - 2);
 	}
-	public int compararLucas(int elemento,int n){
-		if (elemento==funcionLucas(n)) {
-			return elemento;
-		}else if(elemento>funcionLucas(n)){
-			return compararLucas(elemento, n+1);
-		}
+
+	private int compararLucas(int elemento, int n) {
+		if (elemento == this.funcionLucas(n)) return elemento;
+		else if(elemento > this.funcionLucas(n)) return this.compararLucas(elemento, n + 1);
 		return -1;
 	}
-	public int mostrarLucas() {
-		return mostrarLucas(raiz,0);
-	}
-	public int mostrarLucas(Nodo<T> arbol,int cont) {
-		int lucas;
-		if (arbol!=null) {
-			cont=mostrarLucas(arbol.getIzq(),cont);
-			lucas=compararLucas((int)arbol.getElemento(), 0);
-			if (lucas!=-1) {
-				System.out.print(" "+lucas);
+
+	public int mostrarLucas() { return this.mostrarLucas(this.raiz,0); }
+
+	private int mostrarLucas(Nodo<T> arbol, int cont) {
+		if (arbol != null) {
+			cont = this.mostrarLucas(arbol.getIzq(), cont);
+			int lucas = this.compararLucas((int)arbol.getElemento(), 0);
+			if (lucas != -1) {
+				System.out.print(" " + lucas);
 				cont++;
 			}
-			cont=mostrarLucas(arbol.getDer(),cont);
+			cont = this.mostrarLucas(arbol.getDer(), cont);
 		}
 		return cont;
 	}
 	
-	public boolean estaVacio() {
-		if (raiz!=null) {
-			return true;
-		}
-		return false;
-	}
+	public boolean estaVacio() { return this.raiz == null; }
 }
--- Practica 9/src/Presentacion/Test.java
+++ Practica 9/src/Presentacion/Test.java
@@ -6,33 +6,24 @@ import Negocio.*;
 public class Test {
 
 	public static void main(String[] args) {
-		Arbol<Integer> arbolNumerico=new Arbol<Integer>();
+		Arbol<Integer> arbolNumerico = new Arbol<Integer>();
 		llenarArbol(arbolNumerico);
-		if (arbolNumerico.estaVacio()) {
+		if (!arbolNumerico.estaVacio()) {
 			arbolNumerico.imprimirArbol();
 			System.out.println("******SERIE DE LUCAS******");
-			System.out.println("\n Total de nodos: "+arbolNumerico.mostrarLucas());
-		}else {
-			System.out.println("El arbol esta vacio");
-		}
+			System.out.println("\n Total de nodos: " + arbolNumerico.mostrarLucas());
+		}else System.out.println("El arbol esta vacio");
 	}
 	
-	public static void llenarArbol(Arbol<Integer> arbol){
-		ArrayList<String> datos=new ArrayList<String>();
-		String[] linea=null;
-		Archivo Arch=new Archivo();
-		datos=Arch.leer("Datos.txt");
+	private static void llenarArbol(Arbol<Integer> arbol){
+		String[] linea = null;
+		Archivo arch = new Archivo("Datos.txt");
+		ArrayList<String> datos = arch.getDatos();
 		for (int i = 0; i < datos.size(); i++) {
-			linea=datos.get(i).split(",");
-			for (int j = 0; j < linea.length; j++) {
-				if (esNumerico(linea[j])) {
-					arbol.insertar(Integer.parseInt(linea[j]));
-				}
-			}
+			linea = datos.get(i).split(",");
+			for (int j = 0; j < linea.length; j++) if (esNumerico(linea[j])) arbol.insertar(Integer.parseInt(linea[j]));
 		}
 	}
 	
-	public static boolean esNumerico(String Dato) {
-        return Dato.matches("[+-]?\\d*(\\.\\d+)?");
-    }
+	private static boolean esNumerico(String Dato) { return Dato.matches("[+-]?\\d*(\\.\\d+)?"); }
 }
